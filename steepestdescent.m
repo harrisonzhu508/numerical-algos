485ed177037ae30cdfdfaa9880b469ec11800dd5
harrisonzhu508 <harrisonzhu5080@gmail.com>
2017-11-20 10:50:43 +0100

    Steepest Descent Updates
    Updated version

--- steepestdescent.m
+++ steepestdescent.m
@@ -1,29 +1,20 @@
-function x_k = steepestdescent(A, b, x_0, c_1, n, beta)
+function x_k = steepestdescent(A, b, x_0, c_1)
 %%initialisation 
 x_k = x_0;
-%%define beta set and an empty set A
 complexity = 0;
-B = zeros(1,n);
-for i = 1:n
-    B(i) = beta^(i-1);
-end
-%%algorithm
-for k = 1:n
-    p_k = -(A*x_k - b);
-    norm(p_k);
-    if norm(p_k) <= c_1
-        complexity
-        return
-    else
-        %%choose a_k in this list such that the first Wolfe condition is
-        %%satisfied
-        Alpha = zeros(1,n);
-        for i = 1:n
-            Alpha(i)  =  ((0.5*transpose(x_k + B(i)*p_k)*A*(x_k + B(i)*p_k) - transpose(b)*(x_k + B(i)*p_k)) - (0.5*transpose(x_k)*A*x_k - transpose(b)*x_k) <= (c_1*B(i)*transpose(A*x_k - b)*p_k)) * B(i);
-        end
-        a_k = max(Alpha);
-        x_k = x_k + a_k*p_k;
-        complexity = complexity + 1;
+p_k = -(A*x_k - b);
+%%steepest descent algorithmc
+while norm(p_k) > c_1
+    %reinitialise a_k, the step, to 1 each iteration
+    a_k = 1/2;
+    p_k = -(A*x_k - b); %%define the steepest descent direction for each iteration
+    %%choose a_k, the step, in this list such that the first Wolfe condition is
+    %%satisfied
+    while 0.5*(transpose(x_k + a_k*p_k)*A*(x_k + a_k*p_k) - transpose(x_k)*A*x_k) - transpose(b)*(x_k + a_k*p_k - x_k) > c_1*a_k*transpose(A*x_k - b)*p_k
+        a_k = a_k/2;
     end
+    x_k = x_k + a_k*p_k;
+    complexity = complexity + 1;
 end
+complexity
 end
