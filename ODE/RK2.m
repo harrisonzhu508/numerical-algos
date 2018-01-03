df0af6858bc949e144929912b1d25cfad7acb59a
HZ <30379161+harrisonzhu508@users.noreply.github.com>
2018-01-03 12:22:38 +0000

    Add files via upload

--- ODE/RK2.m
+++ ODE/RK2.m
@@ -1,33 +1,15 @@
 %RK2 Implementation on an ODE problem with Van der Pol Oscillator
+%input h, this only does it for 1 time step
 %--------------------------------------------------------------------------------
-function yvals = RK2(T,N,y0)
+function y1 = RK2(h,y)
 %initialisation
-h = T/N;
 func = @(y) [y(2), (1-y(1)^(2))*y(2) - y(1)];
-t_range = linspace(0,T,N);
-yvals = zeros(N,2);
+y1 = [0,0];
 
 %Calculate coefficients and then calculate y(t1) = y(t0 + h) ~ y1
-yvals(1,:) = y0;
-for i = 2:N
-    y = yvals(i-1,:);
-    k1 = func(y);
-    k2 = func(y + h*0.5*k1);
-    yvals(i,:) = y + h*(k1 + k2);
-end
-
-%%plot
-plot(t_range, transpose(yvals(:,1)));
-
-%uncomment for interactive plot
-%curve = animatedline('Color','k');
-%set(gca, 'XLim', [0 T], 'YLim', [-10,10]);
-%grid on;
-%for i = 1:N+1
-%   addpoints(curve, t_range(i),yvals(i,1));
-%   drawnow
-%end
-
+k1 = func(y);
+k2 = func(y + h*0.5*k1);
+y1 = y + h*(k1 + k2);
 
 
 end
