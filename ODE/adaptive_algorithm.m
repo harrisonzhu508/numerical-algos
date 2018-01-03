43355c3210b9496d545da8f4ee0c3f0e6319c03b
HZ <30379161+harrisonzhu508@users.noreply.github.com>
2018-01-03 14:33:15 +0000

    Add files via upload

--- ODE/adaptive_algorithm.m
+++ ODE/adaptive_algorithm.m
@@ -1,26 +1,23 @@
 function yvals = adaptive_algorithm(T,N,y0,TOL)
 %Initialisation
 %Calculate fitted values with T/N
-h =  T/N;
-y1_fitted_RK2 = RK2(h,y0);
-y1_fitted_Euler = Euler(h,y0);
-NORM = norm(y1_fitted_RK2 - y1_fitted_Euler);
-hopt = h*(TOL/NORM)^(1/2);
-h = hopt;
-endpt = 0;
+h = T/N;
+y1_fitted_RK2 = [0,0]
+y1_fitted_Euler = [0,0]
+NORM = 3
+hopt = 0
 c = 1; %keep count of optimal steps
 count = zeros(N,1); %keep track of optimal steps
 yvals = zeros(N,2);
-yvals(1,:) = y0
-y = y1_fitted_Euler;
+yvals(1,:) = y0;
+y = y0;
 
-%while loop for adative algorithm
-while endpt <= T
+%while loop for adaptive algorithm
+while count(c) <= T
     if NORM <= TOL %start new time step with y1 as starting point
         %updates
         c = c + 1; %increase count of steps
         yvals(c,:) = y1_fitted_Euler; %update new values
-        endpt = endpt + hopt;
         count(c) = hopt + count(c-1);
         %new start
         y = y1_fitted_Euler; %new starting point
