32cd2b30af049c80a68c450214c8729fe41da777
HZ <30379161+harrisonzhu508@users.noreply.github.com>
2018-01-03 12:45:41 +0000

    Add files via upload

--- ODE/adaptive_algorithm.m
+++ ODE/adaptive_algorithm.m
@@ -5,19 +5,26 @@ h =  T/N;
 y1_fitted_RK2 = RK2(h,y0);
 y1_fitted_Euler = Euler(h,y0);
 NORM = norm(y1_fitted_RK2 - y1_fitted_Euler);
+hopt = h*(TOL/NORM)^(1/2);
+h = hopt;
 endpt = 0;
-c = 0; %keep count of optimal steps
+c = 1; %keep count of optimal steps
 count = zeros(N,1); %keep track of optimal steps
-hopt = h;
 yvals = zeros(N,2);
-y = [0,0];
+yvals(1,:) = y0
+y = y1_fitted_Euler;
 
 %while loop for adative algorithm
 while endpt <= T
-    h = hopt;
     if NORM <= TOL %start new time step with y1 as starting point
-        y = y1_fitted_Euler; %new starting point
+        %updates
         c = c + 1; %increase count of steps
+        h = hopt;
+        yvals(c,:) = y1_fitted_Euler; %update new values
+        endpt = endpt + hopt;
+        count(c) = hopt;
+        %new start
+        y = y1_fitted_Euler; %new starting point
         y1_fitted_RK2 = RK2(h,y);
         y1_fitted_Euler = Euler(h,y);
         NORM = norm(y1_fitted_RK2 - y1_fitted_Euler);
@@ -29,14 +36,13 @@ while endpt <= T
         hopt = h*(TOL/NORM)^(1/2);
         h = hopt;
     end
-    yvals(1,:) = y1_fitted_Euler;
-    endpt = endpt + hopt;
-    count(c) = hopt;
+end
+
+t_range = linspace(0,T,length(yvals));
+
+plot(t_range, transpose(yvals(:,1)));
 end
         
         
-    
-    
-end
 
 
